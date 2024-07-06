8e68fbaec00636eb8a904a9594fc36b94d32865d
Gobind Kailey <160374905+Gobidesert123@users.noreply.github.com>
2024-07-06 10:01:52 -0400

    testing change

---
+++ Day #2 Tip Calculator.py
@@ -0,0 +1,8 @@
+print("Welcome to the Tip Calculator")
+total_bill = (float(input("What was the total bill? $: ")))
+tip = (int(input("How much tip would you like to give?, 10, 12, 15?")))
+split_between = (int(input("How many people would split the bill?")))
+total = (total_bill * (tip/100) + total_bill)/split_between
+r_total = round(total, 2)
+print(f"Each person should pay: $ {r_total}")
+
